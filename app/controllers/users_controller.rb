c2cc02b8f02aac55b4067edf2fca9e45e6496734
Uma Narayana <uma_senthil@hotmail.com>
2018-02-19 07:02:28 +0000

    Users support implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
-	def index
-	  @users = User.all
-	end
-	
+  def index
+	@users = User.all
+  end
+
+  def create
+	@user = User.create(user_params)
+	@user.save
+
+	redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :user_id, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  get 'users/index'
+  resources :users
 end
